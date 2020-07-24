831c0366fda09463f5233dc854d09a08da0d3f6a
Joao <joao.tonial@sap.com>
2020-07-24 18:51:08 -0300

    Revert "removing code from main.ts"
    This reverts commit 45c41be960e7b29f32c7938b6d3deca074a1f3a2.

--- controllers/main.ts
+++ controllers/main.ts
@@ -1,4 +1,11 @@
 import { Context } from "https://deno.land/x/abc@v1.0.0-rc2/mod.ts";
 import { ErrorHandler } from "../utils/error_middleware.ts";
 
-export const validateRequest = async (ctx: Context, body: any) => {};
+export const validateRequest = async (ctx: Context, body: any) => {
+  if (ctx.request.headers.get("content-type") !== "application/json") {
+    throw new ErrorHandler("Invalid body", 422);
+  }
+  if (!Object.keys(body).length) {
+    throw new ErrorHandler("Request body can not be empty!", 400);
+  }
+};
